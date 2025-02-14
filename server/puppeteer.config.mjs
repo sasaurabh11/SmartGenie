98623a79304b0b4026d313e4a094dea8d5a87dcb
Saurabh <sesaurabh8052@gmail.com>
2025-02-14 23:11:41 +0530

    error: checking for puppeteer problem

--- server/puppeteer.config.mjs
+++
@@ -1,9 +0,0 @@
-import { join } from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const config = { cacheDirectory: join(__dirname, '.cache', 'puppeteer') };
-
-export default config;
