70783b4c73e5666c720b2625fab345ca053ef390
Saurabh <sesaurabh8052@gmail.com>
2025-02-14 23:33:41 +0530

    changed render.sh

--- server/render-build.sh
+++ server/render-build.sh
@@ -4,15 +4,19 @@ set -o errexit
 npm install
 
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+BUILD_CACHE_DIR=/opt/render/project/src/.cache/puppeteer/chrome/
+
 mkdir -p $PUPPETEER_CACHE_DIR
+mkdir -p $BUILD_CACHE_DIR  # Ensure the build cache directory exists
 
 npx puppeteer browsers install chrome
 
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
-    echo "...Copying Puppeteer Cache from Build Cache"
-    # Copying from the actual path where Puppeteer stores its Chrome binary
-    cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
-else
+if [[ -d $PUPPETEER_CACHE_DIR && "$(ls -A $PUPPETEER_CACHE_DIR)" ]]; then
     echo "...Storing Puppeteer Cache in Build Cache"
-    cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+    cp -R $PUPPETEER_CACHE_DIR/* $BUILD_CACHE_DIR
+else
+    echo "...Copying Puppeteer Cache from Build Cache"
+    cp -R $BUILD_CACHE_DIR/* $PUPPETEER_CACHE_DIR
 fi
+
+# npm install && npx puppeteer browsers install chrome
